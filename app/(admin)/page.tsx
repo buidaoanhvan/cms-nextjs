8c1e9b5639e7840970aa6bd9da5098bc5ed46cef
Bùi Đào Anh Văn <buidaoanhvan97@gmail.com>
2024-09-27 12:55:29 +0700

    update seleton

--- app/(admin)/page.tsx
+++ app/(admin)/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           <div className="grid-movies">
             {Array.from({ length: 20 }).map((_, index) => (
               <div key={index} className="animate-pulse flex flex-col">
-                <div className="rounded-md bg-slate-300 h-[371px] w-full mx-auto mb-2"></div>
+                <div className="rounded-md bg-slate-300 h-[19.5vw] w-full mx-auto mb-2"></div>
                 <div className="rounded-md bg-slate-300 h-[28px] w-full mx-auto mb-2"></div>
                 <div className="rounded-md bg-slate-300 h-[63px] w-full mx-auto mb-2"></div>
               </div>
