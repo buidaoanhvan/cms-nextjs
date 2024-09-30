2d58ef11b9d23961a4f33b1fdec627c0cdbd9354
Bùi Đào Anh Văn <buidaoanhvan97@gmail.com>
2024-09-30 11:15:56 +0700

    update

--- app/(auth)/dang-ky/page.tsx
+++ app/(auth)/dang-ky/page.tsx
@@ -131,7 +131,7 @@ export default function RegisterPage() {
             rules={[{ validator: validateOTP }]}
           >
             <Input.OTP
-              formatter={(str) => str.replace(/\D/g, "")}
+              formatter={(str) => str.toUpperCase()}
               length={5}
               size="large"
               style={{ width: "100%" }}
